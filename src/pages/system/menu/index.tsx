c2073f6a1391787601ff537a7fd16e13e59bb47e
xuyang <xuyang@dangbei.com>
2020-06-29 17:45:14 +0800

    132

--- src/pages/system/menu/index.tsx
+++ src/pages/system/menu/index.tsx
@@ -1,10 +1,138 @@
 import React from 'react';
 import styles from './index.css';
+import { Table, Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
 export default () => {
-    return (
-        <div>
-            <h1>菜单管理</h1>
-        </div>
-    );
-}
+  const columns = [
+    {
+      title: '菜单名称',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '图标',
+      dataIndex: 'icon',
+      key: 'icon',
+    },
+    {
+      title: '排序',
+      dataIndex: 'sort',
+      key: 'sort',
+    },
+    {
+      title: '权限标识',
+      dataIndex: 'key',
+      key: 'key',
+    },
+    {
+      title: '组件路径',
+      dataIndex: 'path',
+      key: 'path',
+    },
+    {
+      title: '状态',
+      dataIndex: 'state',
+      key: 'state',
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'create_time',
+      key: 'create_time',
+    },
+  ];
+
+  const data = [
+    {
+      key: 1,
+      name: 'John Brown sr.',
+      children: [
+        {
+          key: 11,
+          name: 'John Brown',
+          age: 42,
+          address: 'New York No. 2 Lake Park',
+        },
+        {
+          key: 12,
+          name: 'John Brown jr.',
+          age: 30,
+          address: 'New York No. 3 Lake Park',
+          children: [
+            {
+              key: 121,
+              name: 'Jimmy Brown',
+              age: 16,
+              address: 'New York No. 3 Lake Park',
+            },
+          ],
+        },
+        {
+          key: 13,
+          name: 'Jim Green sr.',
+          age: 72,
+          address: 'London No. 1 Lake Park',
+          children: [
+            {
+              key: 131,
+              name: 'Jim Green',
+              age: 42,
+              address: 'London No. 2 Lake Park',
+              children: [
+                {
+                  key: 1311,
+                  name: 'Jim Green jr.',
+                  age: 25,
+                  address: 'London No. 3 Lake Park',
+                },
+                {
+                  key: 1312,
+                  name: 'Jimmy Green sr.',
+                  age: 18,
+                  address: 'London No. 4 Lake Park',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+  ];
+
+  const rowSelection = {
+    onChange: (selectedRowKeys: any, selectedRows: any) => {
+      console.log(
+        `selectedRowKeys: ${selectedRowKeys}`,
+        'selectedRows: ',
+        selectedRows,
+      );
+    },
+    onSelect: (record: any, selected: any, selectedRows: any) => {
+      console.log(record, selected, selectedRows);
+    },
+    onSelectAll: (selected: any, selectedRows: any, changeRows: any) => {
+      console.log(selected, selectedRows, changeRows);
+    },
+  };
+  return (
+    <div style={{ padding: 12 }}>
+      <div style={{ marginBottom: 16 }}>
+        <Button type="primary" icon={<PlusOutlined />}>
+          添加菜单
+        </Button>
+      </div>
+      <Table
+        columns={columns}
+        rowSelection={rowSelection}
+        dataSource={data}
+        pagination={false}
+      />
+    </div>
+  );
+};
